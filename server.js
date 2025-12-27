34dfe736af4a4f5bb091a6a1af2ac4b5db8f2597
AlexZx-05 <sharmad7930@gmail.com>
2025-12-27 19:42:04 +0530

    Removed node_modules from repo

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
